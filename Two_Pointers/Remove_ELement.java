dba02c8ace121bd2eb68f477584ecde58a53e7d7
ZaidNaushad <zaidnaushad4@gmail.com>
2026-08-14 20:29:59 +0530

    " Removal of Element"

---
+++ Two_Pointers/Remove_ELement.java
@@ -0,0 +1,16 @@
+package Two_Pointers;
+
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int k=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=val){
+                nums[k]=nums[i];
+                k=k+1;
+
+
+            }
+        }return k;
+
+    }
+}
