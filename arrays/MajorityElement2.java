0d295fc2c122c50838fa6b7cd3998e3c9a06a71f
ZaidNaushad <zaidnaushad4@gmail.com>
2026-08-15 12:08:35 +0530

    "Total Majority ELements Freq>n/3"

---
+++ arrays/MajorityElement2.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        List<Integer> lst=new ArrayList<>();
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            if(map.containsKey(nums[i])){
+                map.put(nums[i],map.get(nums[i])+1);
+            }
+            else{
+                map.put(nums[i],1);
+            }
+        }
+        for(int key: map.keySet()){
+            if(map.get(key)>n/3){
+                lst.add(key);
+            }
+        }
+        return lst;
+    }
+}
